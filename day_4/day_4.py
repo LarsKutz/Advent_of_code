49d6dfa88ffcf97b888629b26f8bd8c32f083bba
Lars K <lars.kutz@t-online.de>
2023-12-04 22:15:17 +0100

    Add solution day 4 part 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -28,5 +28,32 @@ def solution_day_4_1(input_txt):
     return sum(points)
 
 
+
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53 -> 4 (2, 3, 4, 5) additional scratchcards
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19 -> 2 (3, 4) additional scratchcards
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1 -> 2 (4, 5) additional scratchcards
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83 -> 1 (5) additional scratchcards
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36 -> 0 additional scratchcards
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11 -> 0 additional scratchcards
+# -> 30 additional scratchcards
+def solution_day_4_2(input_txt):
+    card_amounts = [1] * len(input_txt)
+
+    for line in input_txt:
+        splitted_line = re.split(r':|\|', line)
+        card_num = int(re.search(r'\d+', splitted_line[0]).group())
+        winning_numbers = set(splitted_line[1].split())
+        my_numbers = set(splitted_line[2].split())
+
+        same_numbers = winning_numbers & my_numbers
+
+        if same_numbers:       
+            for i in range(len(same_numbers)):
+                card_amounts[card_num+i] += card_amounts[card_num-1]
+    
+    return sum(card_amounts)
+
+
 if __name__  == "__main__":
     print(solution_day_4_1(parse_input("input.txt")))
+    print(solution_day_4_2(parse_input("input.txt")))
